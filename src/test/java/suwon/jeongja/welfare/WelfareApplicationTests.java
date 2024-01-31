799ec87034806bfb47c7c9f07e65342cca349744
KimKiHyun0206 <rlarlgus0206@naver.com>
2024-01-31 22:00:27 +0900

    init commit

---
+++ src/test/java/suwon/jeongja/welfare/WelfareApplicationTests.java
@@ -0,0 +1,13 @@
+package suwon.jeongja.welfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelfareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
